287f1bf69f8a923da4ce3f4a15481fec66fddc9f
pooja <poojag@witmates.com>
2018-08-28 19:52:51 +0530

    make admin to approve company

--- app/admin/companies.rb
+++ app/admin/companies.rb
@@ -1,12 +1,13 @@
 ActiveAdmin.register Company do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  
+  member_action :update_status, method: :put do
+    resource.toggle(:approved)
+    resource.save!
+    redirect_to admin_companies_path
+  end
   # permit_params :list, :of, :attributes, :on, :model
   actions :all, except: [:new, :edit]
- 
-
- 
   permit_params :company_name, :company_type, :contact_mail, :company_location, :company_state
   #config.filters = false
   filter :categories, collection: proc {(Category.all).map{|c| [c.category_name, c.id]}}
@@ -26,13 +27,13 @@ ActiveAdmin.register Company do
     column :contact_mail
     column :company_location
     column :company_state
+    # column "Categories Name" do |company|
+    #   company.categories.map(&:category_name).uniq.join(",") 
+    # end
     column :approved
-    column "Categories Name" do |company|
-      company.categories.map(&:category_name).uniq.join(",") 
+    actions do |resource|
+      text = resource.approved? ? 'Unapprove' : 'Approve'
+      link_to text, update_status_admin_company_path(resource), method: :put
     end
-    actions
-    end
-
-  
-
+  end
 end
